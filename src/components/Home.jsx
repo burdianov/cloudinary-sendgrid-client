810e32c9c32e7f4ed9f7d1aff439a6a825ebfb14
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-12-29 21:23:32 +0400

    refactoring

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Layout from "./Layout";
+import {Helmet} from "react-helmet/es/Helmet";
 
 const Home = () => {
   return (
     <Layout>
+      <Helmet>
+        <title>MERN Stack</title>
+        <meta name="description"
+              content="MERN Stack React Cloudinary Sendgrid SSR App"/>
+      </Helmet>
       <div className="container text-center">
         <h1 className="p-5">Home</h1>
         <hr/>
-        <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, illum.</p>
+        <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Eos, illum.</p>
       </div>
     </Layout>
   );
